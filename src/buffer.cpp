dcfa39fa78782b7c38354299dff375dede8fe450
Chris Galardi <ixplode@gmail.com>
2013-05-13 22:57:21 -0400

    added buffer

---
+++ src/buffer.cpp
@@ -0,0 +1,23 @@
+//
+//  buffer.cpp
+//  SpaceJunk
+//
+//  Created by ixtli on 5/13/13.
+//  Copyright (c) 2013 ixtli. All rights reserved.
+//
+
+#include "buffer.h"
+
+Buffer::Buffer()
+{}
+
+Buffer::~Buffer()
+{
+	
+}
+
+bool Buffer::init()
+{
+	
+	return true;
+}
---
+++ src/buffer.h
@@ -0,0 +1,27 @@
+//
+//  buffer.h
+//  SpaceJunk
+//
+//  Created by ixtli on 5/13/13.
+//  Copyright (c) 2013 ixtli. All rights reserved.
+//
+
+#ifndef __SpaceJunk__buffer__
+#define __SpaceJunk__buffer__
+
+class Buffer
+{
+public:
+	
+	Buffer();
+	~Buffer();
+	
+	bool init();
+	
+private:
+	
+	
+	
+};
+
+#endif /* defined(__SpaceJunk__buffer__) */
