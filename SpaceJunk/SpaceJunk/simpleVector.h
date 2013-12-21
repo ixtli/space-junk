9e2a38dbc7d56a3cbf25e00356522dc57a2ee183
ixtli <christophergalardi@gmail.com>
2013-12-21 14:33:16 -0500

    added simple vector and removed unnecessary files. also reorganized with a util folder

---
+++ SpaceJunk/SpaceJunk/simpleVector.h
@@ -0,0 +1,24 @@
+//
+//  simpleVector.h
+//  SpaceJunk
+//
+//  Created by ixtli on 12/20/13.
+//  Copyright (c) 2013 ixtli. All rights reserved.
+//
+
+#ifndef SpaceJunk_simpleVector_h
+#define SpaceJunk_simpleVector_h
+
+template <typename T>
+class SimpleVector
+{
+public:
+	
+	SimpleVector();
+	~SimpleVector();
+	
+private:
+	
+};
+
+#endif
--- src/engine/ringBuffer.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ringBuffer.cpp
-//  SpaceJunk
-//
-//  Created by ixtli on 11/18/13.
-//  Copyright (c) 2013 ixtli. All rights reserved.
-//
-
-#include "ringBuffer.h"
